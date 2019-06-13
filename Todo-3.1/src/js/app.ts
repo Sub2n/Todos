ba33b00191ec82f176178c2e85c5fdd9f82f9bb2
Sub2n <95su1208@gmail.com>
2019-06-13 11:16:32 +0900

    init

--- Todo-3.1/src/js/app.ts
+++ Todo-3.1/src/js/app.ts
@@ -1,3 +1,14 @@
 class TodoList {
+  private $inputTodo: HTMLInputElement = document.querySelector('.input-todo');
+  private $todos: HTMLUListElement = document.querySelector('.todos');
+  private $completeAll: HTMLInputElement = document.querySelector('#ck-complete-all');
+  private $clearCompleted: HTMLDivElement = document.querySelector('.clear-completed');
+  private $completedTodos = document.querySelector('.completed-todos');
+  private $activeTodos = document.querySelector('.active-todos');
+  private $nav = document.querySelector('.nav');
+  private $spinner = document.querySelector('.spinner');
+
+  private menuFlag = 'all';
+  private todos = [];
   
 }
