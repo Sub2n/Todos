e9f67aeec32d085808e75b180ec1d71141f05661
Sub2n <95su1208@gmail.com>
2019-06-21 14:33:43 +0900

    pipe

--- Todo4/src/app/filter.pipe.ts
+++ Todo4/src/app/filter.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Todo } from './todo.interface';
+import { NavItem } from './nav-item.type';
 
 @Pipe({
   name: 'filter'
   })
 export class FilterPipe implements PipeTransform {
-  transform(todos: Todo[], active: string): any {
+  transform(todos: Todo[], active: NavItem): any {
     if (active === 'All') return todos;
     return active === 'Active'
       ? todos.filter(todo => !todo.completed)
